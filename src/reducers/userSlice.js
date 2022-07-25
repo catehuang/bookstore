a3718f796d86c9ecd4e94889e997860fc2045911
s0350228@gmail.com <s0350228@gmail.com>
2022-07-24 23:18:09 -0600

    add required actions and states to complete tasks

--- src/reducers/userSlice.js
+++ src/reducers/userSlice.js
@@ -1,49 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
-
-
-
 const userSlice = createSlice({
   name: "user",
   initialState: {
     currentUser: null,
-    isFeteching: false,
+    isFetching: false,
     loginError: false,
     registerError: false,
     registered: false,
+    registerErrorCode: 1
   },
   reducers: {
     registerSuccess: (state, action) => {
-      state.registered= true;
       state.currentUser = action.payload;
+      state.registered = true;
     },
-    registerFailure: (state) => {
-      state.isFeteching = false;
+    registerFailure: (state, action) => {
       state.registerError = true;
+      state.registerErrorCode = action.payload;
     },
     loginStart: (state) => {
       state.isFetching = true;
     },
-    loginSuccess: (state, action) =>{
-        state.isFetching = false;
-        state.currentUser = action.payload;
-      },
+    loginSuccess: (state, action) => {
+      state.isFetching = false;
+      state.currentUser = action.payload;
+    },
     loginFailure: (state) => {
-      state.isFeteching = false;
+      state.isFetching = false;
       state.loginError = true;
     },
-    logout:(state) => {
-        state.currentUser = null;
-        state.registered = false;
-        state.loginError = false;
-        state.registerError = false;
-      },
+    logout: (state) => {
+      state.currentUser = null;
+      state.isFetching = false;
+      state.loginError = false;
+      state.registered = false;
+      state.registerError = false;
+    },
   },
 });
 
-export const { registerSuccess, registerFailure, loginStart, loginSuccess, loginFailure, logout } =
-  userSlice.actions;
+export const {
+  registerSuccess,
+  registerFailure,
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  logout,
+} = userSlice.actions;
 export default userSlice.reducer;
-
