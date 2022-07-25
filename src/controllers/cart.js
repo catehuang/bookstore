155f0739f3d7fc125434b96bdf990d0e0f9edeeb
s0350228@gmail.com <s0350228@gmail.com>
2022-07-25 17:08:39 -0600

    add cart requests

---
+++ src/controllers/cart.js
@@ -0,0 +1,39 @@
+import { publicRequest } from "../publicRequest";
+import { newCart, setCart } from "../reducers/cartSlice";
+
+export const CreateCart = async (dispatch, userId) => {
+        try {
+                const response = await publicRequest.post(`carts/new/${userId}`, {
+                        userId: userId,
+                });
+                dispatch(newCart(response.data));
+        } catch (error) {
+                console.log(error);
+        }
+};
+
+export const LoadCart = async (dispatch, userId) => {
+        try {
+                const response = await publicRequest.get(`/carts/find/${userId}`);
+
+                //console.log(response.data);
+                //console.log(userId);
+                //if user doen't have any cart, create a new cart
+                if (response.data === null) {
+                        CreateCart(dispatch, userId);
+                        //console.log(userId);
+                } else {
+                        console.log(response.data.userId);
+                        dispatch(setCart(response.data));
+                }
+        } catch (error) { }
+};
+
+export const UpdateCart = async (cart) => {
+        try {
+                const response = await publicRequest.put(`/carts/${cart._id}`, {
+                        body: cart,
+                });
+                console.log(response.data);
+        } catch (error) { }
+};
