a8fc010c97c7ade8c15e58e23cd5c24c628743cc
s0350228@gmail.com <s0350228@gmail.com>
2022-07-27 11:47:07 -0600

    adjust for responsive

--- src/components/EmptyCart.jsx
+++ src/components/EmptyCart.jsx
@@ -8,7 +8,7 @@ function EmptyCart() {
                 <div className="grow bg-white rounded-lg p-10">
                         <div className="flex flex-row">
                                 <img
-                                        className="h-72"
+                                        className="max-h-60 object-scale-down lg:object-contain"
                                         src="https://m.media-amazon.com/images/G/15/cart/empty/kettle-desaturated._CB424694027_.svg"
                                         alt=""
                                 />
@@ -18,15 +18,15 @@ function EmptyCart() {
                                         <Link to="/">
                                                 <p className="text-cyan-600 mt-2 mb-5">Shop today's deals</p>
                                         </Link>
-                                        <div className="flex justify-around mx-auto">
+                                        <div className="grid grid-cols-2 gap-3">
                                                 <Link to="/login">
-                                                        <button className="border border-yellow-300 bg-amber-200 w-full rounded hover:bg-amber-300 py-1 px-4">
-                                                                Sign in your account
+                                                        <button className="border border-yellow-300 bg-amber-200 w-full rounded hover:bg-amber-300 py-1 px-4 flex-none">
+                                                                Sign in
                                                         </button>
                                                 </Link>
                                                 <Link to="/register">
-                                                        <button className="border border-gray-300 bg-white w-full rounded hover:bg-amber-300 py-1 px-4">
-                                                                Sign up now
+                                                        <button className="border border-gray-300 bg-white w-full rounded hover:bg-amber-300 py-1 px-4 flex-none">
+                                                                Sign up
                                                         </button>
                                                 </Link>
                                         </div>
