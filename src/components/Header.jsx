1cc9e451abe4c15cc31d5c158f74ee2bd3dd87e5
s0350228@gmail.com <s0350228@gmail.com>
2022-07-26 19:42:02 -0600

    show the number of cart items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,43 +3,58 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Badge from "@material-ui/core/Badge";
 
 function Header() {
-  const user = useSelector(state => state.user.currentUser);
+  const user = useSelector((state) => state.user.currentUser);
+  const cart = useSelector((state) => state.cart.products.length);
 
   return (
     <div className="px-5 py-3 bg-[#00131a] text-gray-200">
       <ul className="flex gap-10">
         <li className="text-xl first-letter:font-bold">
-        <Link to="/">BookStore</Link>
+          <Link to="/">BookStore</Link>
         </li>
         <li className="mx-auto grow">
           <div className="flex">
             <input type="text" className="rounded-l-lg text-[#00131a] px-2" />
-            <p className="border-t border-r border-b rounded-r-lg px-1 bg-amber-400 border-none text-[#00131a]"><SearchIcon /></p>
+            <p className="border-t border-r border-b rounded-r-lg px-1 bg-amber-400 border-none text-[#00131a]">
+              <SearchIcon />
+            </p>
           </div>
         </li>
         <li className="">
           <div>
-              {
-                user
-                ? <p>Hello  {user.username}</p>
-                : <Link to="/login"><p>Login</p></Link>
-              }
+            {user ? (
+              <p>Hello {user.username}</p>
+            ) : (
+              <Link to="/login">
+                <p>Login</p>
+              </Link>
+            )}
           </div>
-          </li>
+        </li>
         <li className="flex-none">
-        {
-                user
-                ? <Link to="/logout"><p>Logout</p></Link>
-                : <Link to="/register"><p>Sign up</p></Link>
-              }
-         </li>
-        <li className="text-2xs">
-        <ShoppingCartIcon/>
+          {user ? (
+            <Link to="/logout">
+              <p>Logout</p>
+            </Link>
+          ) : (
+            <Link to="/register">
+              <p>Sign up</p>
+            </Link>
+          )}
+        </li>
+        <li className="text-2xs flex-none flex">
+          <Link to="checkout">
+            <Badge badgeContent={ `${cart}` } color="error">
+              <ShoppingCartIcon />
+            </Badge>
+          </Link>
         </li>
       </ul>
     </div>
-)}
+  );
+}
 
 export default Header;
