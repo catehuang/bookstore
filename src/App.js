645cd221dbfc12e4eda067a4e6ceb75073bfe9ea
s0350228@gmail.com <s0350228@gmail.com>
2022-07-26 19:40:44 -0600

    add checkout route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Book from "./pages/Book";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Checkout from "./pages/Checkout";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { useSelector } from "react-redux";
@@ -16,9 +17,10 @@ function App() {
     <BrowserRouter>
     <Header />
       <Routes>
+      <Route path="/checkout" element={<Checkout/>}></Route>
        <Route path="/logout" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/login" element={ user? <Navigate to="/" /> : <Login/>}></Route>
+        <Route path="/register" element={ user ? <Navigate to="/" /> : <Register/>}></Route>
         <Route path="/books/:id" element={<Book/>}></Route>
         <Route path="/" element={<Home />}></Route>
       </Routes>
