8ec74bdef039d464d317686cac717488f6807c40
s0350228@gmail.com <ya-yun.huang@edu.sait.ca>
2022-08-07 14:55:53 -0600

    cors

--- backend/server.js
+++ backend/server.js
@@ -51,6 +51,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors());
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
