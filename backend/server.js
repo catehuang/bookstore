9b99ea6898055924d3e90e5a6cb17899403c32bb
s0350228@gmail.com <s0350228@gmail.com>
2022-10-17 17:21:05 -0600

    modify settings for heroku

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === "production") {
     });
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
     });
 }
 
