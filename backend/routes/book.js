01e66a17edeb7459fa8c66da8bfcd793ad0d2bc4
s0350228@gmail.com <s0350228@gmail.com>
2022-10-17 17:14:18 -0600

    modify settings for heroku

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,6 +1,7 @@
 const Book = require("../models/book");
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../middlewares/authJwt");
 
 // Get ALL Books 
 router.get("/",  async (req, res) => {
@@ -24,7 +25,7 @@ router.get("/find/:id", async (req, res) => {
 
 
 //CREATE
-// router.post("/", verifyTokenAndAdmin, async (req, res) => {
+// router.post("/", verifyToken, async (req, res) => {
 //   const newProduct = new Product(req.body);
 //   try {
 //     const savedProduct = await newProduct.save();
@@ -34,24 +35,24 @@ router.get("/find/:id", async (req, res) => {
 //   }
 // });
 
-//UPDATE
-// router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     const updatedProduct = await Product.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body, //accepts all from body
-//       },
-//       { new: true } //return object to updateUser
-//     );
-//     res.status(200).json(updatedProduct);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// UPDATE
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body, //accepts all from body
+      },
+      { new: true } //return object to updateUser
+    );
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //DELETE
-// router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+// router.delete("/:id", verifyToken, async (req, res) => {
 //   try {
 //     await Product.findByIdAndDelete(req.params.id);
 //     res.status(200).json("Product has been deleted.");
--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,10 @@
 import axiosLib from "axios";
 
 // for development
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
 
 // for deployment
-// const BASE_URL = "https://web-app-bookstore.herokuapp.com/api";
+const BASE_URL = "https://web-app-bookstore.herokuapp.com/api";
 
 export const axios = axiosLib.create({
   baseURL: BASE_URL,
