afd456801fa231f3a88d0e60d148484b56181622
s0350228@gmail.com <s0350228@gmail.com>
2022-10-18 10:38:40 -0600

    type

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "production") {
     next();
   });
 
-  pp.use(express.static("build"));
+  app.use(express.static("build"));
   
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../build", "index.html"));
