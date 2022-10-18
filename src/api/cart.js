9d75449b33b1ec89ee942a16afe119f37027a474
s0350228@gmail.com <s0350228@gmail.com>
2022-10-18 11:16:56 -0600

    close log

--- src/api/cart.js
+++ src/api/cart.js
@@ -11,8 +11,8 @@ export const LoadCart = async (dispatch, user) => {
             headers: { "x-access-token": `${token}` },
         });
         const response = await axiosAuth.get(`/carts/find/${userId}`);
-        console.log("load cart =============");
-        console.log(response.data);
+        // console.log("load cart =============");
+        // console.log(response.data);
 
         if (response.data === null) {
             CreateCart(dispatch, user);
