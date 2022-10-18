96a1455ecf3b91500e2d43c1880eb4ab54e05990
s0350228@gmail.com <s0350228@gmail.com>
2022-10-17 22:31:41 -0600

    modify settings

--- backend/server.js
+++ backend/server.js
@@ -12,48 +12,45 @@ const path = require("path");
 const url = process.env.MONGO_URL;
 
 require("dotenv").config();
-
-mongoose
-    .connect("mongodb+srv://" + url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Successfully connect to MongoDB."))
-    .catch((err) => console.error("connection error", err.stack));
-
-const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV !== "production") {
-    console.log("Dev Mode");
+mongoose
+  .connect("mongodb+srv://" + url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Successfully connect to MongoDB."))
+  .catch((err) => console.error("connection error", err.stack));
 
-    const corsOptions = {
-        credentials: true,
-        origin: true,
-    };
-    app.use(cors(corsOptions));
-    app.use(express.static(__dirname + "/public"));
+if (process.env.NODE_ENV !== "production") {
+  const corsOptions = {
+    credentials: true,
+    origin: true,
+  };
+  app.use(cors(corsOptions));
+  app.use(express.static(__dirname + "/public"));
 }
 
 if (process.env.NODE_ENV === "production") {
-    console.log("Production Mode");
-    const buildPath = path.join(__dirname, '..', 'build');
-    app.use(express.static(buildPath));
-    app.use(express.static('build'));
+  const buildPath = path.join(__dirname, "..", "build");
+  app.use(express.static(buildPath));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-    });
+  app.use(cors());
 
-    app.use(cors());
-    app.all("/*", function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Credentials", true);
-        res.header("Access-Control-Allow-Credentials", "GET,PUT,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        next();
-    });
+  app.all("/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
+  pp.use(express.static("build"));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+  });
 }
 
 app.use("/api", userRoute);
@@ -63,5 +60,5 @@ app.use("/api/orders", orderRoute);
 app.use("/api/payments", paymentRoute);
 
 app.listen(PORT, () => {
-    console.log(`Sever is running on the port: `, PORT);
+  console.log(`Sever is running on the port: `, PORT);
 });
