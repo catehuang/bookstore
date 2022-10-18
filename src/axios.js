cc61ad3a8878dd75197e9c3996ded8006a19aa47
s0350228@gmail.com <s0350228@gmail.com>
2022-10-18 13:43:47 -0600

    test

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import axiosLib from "axios";
 // const BASE_URL = "http://localhost:5000/api";
 
 // for deployment
-const BASE_URL = "https://web-app-bookstore.herokuapp.com/api";
+const BASE_URL = "/api";
 
 export const axios = axiosLib.create({
   baseURL: BASE_URL,
