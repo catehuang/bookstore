d5a2687eed56adec737afcec53af0db1f72256b7
s0350228@gmail.com <s0350228@gmail.com>
2022-10-18 21:18:31 -0600

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -51,12 +51,8 @@ if (process.env.NODE_ENV === "production") {
     app.all("/*", function (req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
         res.header("Access-Control-Allow-Credentials", true);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Credentials', 'GET,PUT,POST,DELETE,OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-        // res.header("Access-Control-Allow-Origin", "*");
-        // res.header("Access-Control-Allow-Credentials", true);
-        // res.header("Access-Control-Allow-Credentials", "GET,PUT,POST,DELETE");
-        // res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
     });
 
