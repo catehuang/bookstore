e869f8d25afec32d5df3ad0dd6d76a20c27f997c
s0350228@gmail.com <s0350228@gmail.com>
2022-10-19 15:06:40 -0600

    fixed search function and split it into a new component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Badge from "@mui/material/Badge";
 import { logoutCart, clearCart } from "../reducers/cartSlice";
@@ -9,17 +8,14 @@ import { UserLogout } from "../api/user";
 import { logout } from "../reducers/userSlice";
 import { UpdateCart } from "../api/cart";
 import { axios } from "../axios";
-import { ContactPageOutlined } from "@mui/icons-material";
+import SearchBar from "./SearchBar"
 
 function Header() {
     const user = useSelector((state) => state.user.currentUser);
     const cart = useSelector((state) => state.cart);
     const [books, setBooks] = useState([]);
-    const [searchString, setSearchString] = useState("");
     const quantity = cart.quantity;
     const dispatch = useDispatch();
-    const [isOpen, setIsOpen] = useState(false);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const getBooks = async () => {
@@ -47,72 +43,13 @@ function Header() {
         dispatch(clearCart());
     };
 
-    const handleSearch = (str) => {
-        setSearchString(str.replace(/[^0-9a-z]/gi, ""));
-        setIsOpen(true);
-    };
-
-    const handleClickSearch = (bookId) => {
-        setSearchString("");
-        setIsOpen(false);
-        navigate(`/books/${bookId}`);
-    };
-
-    const SearchResult = () => {
-        return (
-            <div className="w-4/5 md:w-1/3 bg-white mt-5 px-2 rounded text-gray-700 border border-gray-200 rouned-lg text-sm flex flex-col gap-2 absolute z-50 ">
-                {searchString === ""
-                    ? setIsOpen(false)
-                    : books
-                        .filter((book) => {
-                            if (
-                                book.name.toLowerCase().includes(searchString.toLowerCase())
-                            )
-                                return book;
-                            else return null;
-                        })
-                        ?.map((book) => (
-                            <p
-                                className="flex-wrap "
-                                key={book._id}
-                                onClick={() => handleClickSearch(`${book._id}`)}
-                            >
-                                <span className="cursor-pointer line-clamp-1">
-                                    {book.name}
-                                </span>
-                            </p>
-                        ))}
-            </div>
-        );
-    };
-
-    const SearchBar = () => {
-        return (
-            <div id="searchBar" className="">
-                <div className="flex">
-                    <input
-                        type="text"
-                        className="rounded-none rounded-l  text-[#00131a] px-2 w-full md:w-52"
-                        value={searchString}
-                        onChange={(e) => handleSearch(e.target.value)}
-                        onBlur={(e) => setIsOpen(false)}
-                    />
-                    <p className="border border-amber-400 rounded-r bg-amber-400 text-[#00131a] px-1">
-                        <SearchIcon />
-                    </p>
-                </div>
-                <div id="searchResult">{isOpen && <SearchResult />}</div>
-            </div>
-        );
-    };
-
     return (
         <div className="bg-[#00131a] text-gray-200 px-10 py-5 flex flex-wrap">
-            <div id="bookstorte_searchBar" className="flex space-x-10">
+            <div id="bookstore_searchBar" className="flex space-x-10">
                 <div className="text-xl first-letter:font-bold">
                     <Link to="/">BookStore</Link>
                 </div>
-                <div className="hidden md:block">{SearchBar()}</div>
+                <div id="searchBar_normal" className="hidden md:block"><SearchBar books={books}/></div>
             </div>
             <div id="userInfo" className="flex ml-auto space-x-5 md:space-x-10 my-auto">
                 <div id="login_username" className="">
@@ -151,7 +88,7 @@ function Header() {
                     </Link>
                 </div>
             </div>
-            <div className="md:hidden w-full mt-5">{SearchBar()}</div>
+            <div id="searchBar_mobile" className="md:hidden w-full mt-5"><SearchBar books={books}/></div>
         </div>
     );
 }
